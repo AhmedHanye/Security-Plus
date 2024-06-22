edd755caec16cc3495b83584ab54dbbc8de82418
Ahmed Hanye <s22512332eg@std.aou.edu.eg>
2024-06-22 22:54:27 +0300

    update action and gsap animation
    the problem is that the action wasn't working with gsap before i select start scan neither after it

--- Extension/src/components/virustotal.tsx
+++ Extension/src/components/virustotal.tsx
@@ -22,6 +22,7 @@ const Virsustotal = ({
   }>({});
   const [result, setResult] = useState<any>({});
   const [loading, setLoading] = useState(true);
+  const [startScan, setStartScan] = useState(false);
   const handleScanURL = () => {
     scanURL(url, (result: any) => {
       if (!result[1]) {
@@ -40,116 +41,144 @@ const Virsustotal = ({
     });
   };
   useEffect(() => {
-    if (url !== "") {
+    if (url !== "" && startScan) {
       handleScanURL();
     }
   }, [url]);
   return (
-    
     <section
       className={
         "mx-8 min-h-48 max-md:min-h-64 max-md:m-0 max-md:rounded-none rounded-3xl flex max-md:flex-col overflow-hidden shadow-2xl shadow-black bg-Charcoal dark:bg-Gainsboro text-base relative"
       }
     >
-      <div
-        className={
-          "w-full p-6 max-md:p-5 flex items-center max-md:justify-center gap-4"
-        }
-      >
-        <div
-          id={"final-result"}
-          className={
-            "flex items-center justify-center relative rounded-full w-32 h-32 bg-white shadow-inner shadow-black"
-          }
-        >
-          <span className={"font-bold text-xl"}>
-            {result["malicious"] == undefined ? "--" : result["malicious"]} /{" "}
-            {Object.keys(result).length !== 0
-              ? result["malicious"] +
-                result["suspicious"] +
-                result["harmless"] +
-                result["undetected"]
-              : "--"}
-          </span>
-        </div>
-        <div
-          className={
-            "grid grid-cols-2 grid-rows-2 max-lg:grid-cols-1 max-lg:grid-rows-4 gap-x-6 max-md:gap-0 h-full items-center font-extrabold capitalize max-lg:text-sm max-md:h-24"
-          }
-        >
-          <span className={"dark:text-red-600 text-red-500"}>
-            <pre>
-              {"malicious".padEnd(11, " ")}:{" "}
-              {result["malicious"] == undefined ? "--" : result["malicious"]}
-            </pre>
-          </span>
-          <span className={"dark:text-yellow-600 text-yellow-400"}>
-            <pre>
-              {"suspicious".padEnd(11, " ")}:{" "}
-              {result["suspicious"] == undefined ? "--" : result["suspicious"]}
-            </pre>
-          </span>
-          <span className={"dark:text-gray-600 text-gray-300"}>
-            <pre>
-              {"undetected".padEnd(11, " ")}:{" "}
-              {result["undetected"] == undefined ? "--" : result["undetected"]}
-            </pre>
-          </span>
-          <span className={"dark:text-green-600 text-green-400"}>
-            <pre>
-              {"harmless".padEnd(11, " ")}:{" "}
-              {result["harmless"] == undefined ? "--" : result["harmless"]}
-            </pre>
-          </span>
-        </div>
-      </div>
-      <div
-        className={`overflow-auto database-scrollbar transform translate-x-2 h-full max-h-48 w-full dark:bg-gray-300 bg-gray-800 ${
-          Object.keys(details).length == 0 ? "hidden" : ""
-        }`}
-      >
-        <table className="w-full text-left text-xs rtl:text-right">
-          <thead className="dark:text-gray-700 uppercase dark:bg-gray-50 bg-gray-700 text-gray-100 sticky top-0">
-            <tr>
-              <th className="px-6 py-3">engine</th>
-              <th className="px-6 py-3">result</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(details).map((key) => (
-              <tr
-                key={key}
-                className={
-                  "border-b border-gray-700  dark:hover:bg-gray-50 hover:bg-gray-600 text-sm"
-                }
-              >
-                <td className="font-medium ps-6 py-1 dark:text-gray-900 whitespace-nowrap text-white">
-                  {details[key].engine_name}
-                </td>
-                <td className={" text-white ps-7 py-1 dark:text-black"}>
-                  {details[key].result}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <img
-        src={virustotalImage}
-        className={"max-md:hidden"}
-        alt="virustotal Image"
-      />
-      {loading && (
-        <div
-          className={
-            "absolute top-0 w-full h-full flex items-center justify-center bg-Charcoal dark:bg-gray-400"
-          }
-        >
-          <FontAwesomeIcon
-            icon={faCircleNotch}
-            className={"text-white animate-spin text-3xl"}
+      {!startScan ? (
+        <>
+          <div className={"w-full h-full flex justify-center items-center"}>
+            <button
+              type="button"
+              class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-bold rounded-md text-lg px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+              onClick={() => setStartScan(true)}
+            >
+              Start Scan
+            </button>
+          </div>
+          <img
+            src={virustotalImage}
+            className={"max-sm:hidden h-full"}
+            alt="virustotal Image"
           />
-        </div>
+        </>
+      ) : (
+        <>
+          <div
+            className={
+              "w-full p-6 max-md:p-5 flex items-center max-md:justify-center gap-4"
+            }
+          >
+            <div
+              id={"final-result"}
+              className={
+                "flex items-center justify-center relative rounded-full w-32 h-32 bg-white shadow-inner shadow-black"
+              }
+            >
+              <span className={"font-bold text-xl"}>
+                {result["malicious"] == undefined ? "--" : result["malicious"]}{" "}
+                /{" "}
+                {Object.keys(result).length !== 0
+                  ? result["malicious"] +
+                    result["suspicious"] +
+                    result["harmless"] +
+                    result["undetected"]
+                  : "--"}
+              </span>
+            </div>
+            <div
+              className={
+                "grid grid-cols-2 grid-rows-2 max-lg:grid-cols-1 max-lg:grid-rows-4 gap-x-6 max-md:gap-0 h-full items-center font-extrabold capitalize max-lg:text-sm max-md:h-24"
+              }
+            >
+              <span className={"dark:text-red-600 text-red-500"}>
+                <pre>
+                  {"malicious".padEnd(11, " ")}:{" "}
+                  {result["malicious"] == undefined
+                    ? "--"
+                    : result["malicious"]}
+                </pre>
+              </span>
+              <span className={"dark:text-yellow-600 text-yellow-400"}>
+                <pre>
+                  {"suspicious".padEnd(11, " ")}:{" "}
+                  {result["suspicious"] == undefined
+                    ? "--"
+                    : result["suspicious"]}
+                </pre>
+              </span>
+              <span className={"dark:text-gray-600 text-gray-300"}>
+                <pre>
+                  {"undetected".padEnd(11, " ")}:{" "}
+                  {result["undetected"] == undefined
+                    ? "--"
+                    : result["undetected"]}
+                </pre>
+              </span>
+              <span className={"dark:text-green-600 text-green-400"}>
+                <pre>
+                  {"harmless".padEnd(11, " ")}:{" "}
+                  {result["harmless"] == undefined ? "--" : result["harmless"]}
+                </pre>
+              </span>
+            </div>
+          </div>
+          <div
+            className={`overflow-auto database-scrollbar transform translate-x-2 h-full max-h-48 w-full dark:bg-gray-300 bg-gray-800 ${
+              Object.keys(details).length == 0 ? "hidden" : ""
+            }`}
+          >
+            <table className="w-full text-left text-xs rtl:text-right">
+              <thead className="dark:text-gray-700 uppercase dark:bg-gray-50 bg-gray-700 text-gray-100 sticky top-0">
+                <tr>
+                  <th className="px-6 py-3">engine</th>
+                  <th className="px-6 py-3">result</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(details).map((key) => (
+                  <tr
+                    key={key}
+                    className={
+                      "border-b border-gray-700  dark:hover:bg-gray-50 hover:bg-gray-600 text-sm"
+                    }
+                  >
+                    <td className="font-medium ps-6 py-1 dark:text-gray-900 whitespace-nowrap text-white">
+                      {details[key].engine_name}
+                    </td>
+                    <td className={" text-white ps-7 py-1 dark:text-black"}>
+                      {details[key].result}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <img
+            src={virustotalImage}
+            className={"max-md:hidden"}
+            alt="virustotal Image"
+          />
+          {loading && (
+            <div
+              className={
+                "absolute top-0 w-full h-full flex items-center justify-center bg-Charcoal dark:bg-gray-400"
+              }
+            >
+              <FontAwesomeIcon
+                icon={faCircleNotch}
+                className={"text-white animate-spin text-3xl"}
+              />
+            </div>
+          )}
+        </>
       )}
     </section>
   );
--- Extension/src/pages/app.tsx
+++ Extension/src/pages/app.tsx
@@ -9,14 +9,12 @@ import Virsustotal from "../components/virustotal";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-import virustotalImage from "../assets/virustotal.svg";
-
 export const App = () => {
   const [url, setUrl] = useState("");
   const [notifications, setNotifications] = useState<string[]>([]);
-  const [virustotal, setVirustotal] = useState(false);
+  const [virustotal, setVirustotal] = useState(true);
   const [theme, setTheme] = useState(null);
-  const [startScan, setStartScan] = useState(false);
+  
   useEffect(() => {
     getPreference((data) => {
       if (data !== undefined) setVirustotal(data);
@@ -56,7 +54,7 @@ export const App = () => {
 
   useGSAP(() => {
     const tl = gsap.timeline();
-    tl.from("section", { opacity: 0, y: 50, duration: 0.5 });
+    tl.from("section", { opacity: 0, y: 50,stagger:0.2, duration: 0.5, ease: "power2.out"});
   }, []);
 
   return (
@@ -68,31 +66,9 @@ export const App = () => {
         }
       >
         <Action url={url} addNotification={addNotification} />
-        {virustotal &&
-          (startScan ? (
-            <Virsustotal url={url} addNotification={addNotification} />
-          ) : (
-            <div
-              className={
-                "mx-8 min-h-48 overflow-hidden max-md:min-h-64 flex max-md:m-0 max-md:rounded-none rounded-3xl shadow-2xl shadow-black bg-Charcoal dark:bg-Gainsboro"
-              }
-            >
-              <div className={"w-full h-full flex justify-center items-center"}>
-                <button
-                  type="button"
-                  class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-bold rounded-md text-lg px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-                  onClick={() => setStartScan(true)}
-                >
-                  Start Scan
-                </button>
-              </div>
-              <img
-                src={virustotalImage}
-                className={"h-full"}
-                alt="virustotal Image"
-              />
-            </div>
-          ))}
+        {virustotal && (
+          <Virsustotal url={url} addNotification={addNotification} />
+        )}
         <Notification
           message={notifications[0]}
           numMessages={notifications.length - 1}
